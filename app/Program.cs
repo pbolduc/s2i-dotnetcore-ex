34e3442d9863c9e8b6a6608c82bbf67dd1ad1fa6
Phil Bolduc <PhilBolduc@gmail.com>
2021-06-18 13:24:36 -0700

    Update Program.cs
    add thread pool stats

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +15,26 @@ namespace app
         public static void Main(string[] args)
         {
             Console.WriteLine("CPUs: {0}", Environment.ProcessorCount);
+          
+            ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxIoThreads);
+            ThreadPool.GetAvailableThreads(out int freeWorkerThreads, out int freeIoThreads);
+            ThreadPool.GetMinThreads(out int minWorkerThreads, out int minIoThreads);
+          
+            int busyIoThreads = maxIoThreads - freeIoThreads;
+            int busyWorkerThreads = maxWorkerThreads - freeWorkerThreads;
+
+            Console.WriteLine("worker_threads_min: {0}", minWorkerThreads);
+            Console.WriteLine("worker_threads_max: {0}", maxWorkerThreads);
+
+            Console.WriteLine("worker_threads_free: {0}", freeWorkerThreads);
+            Console.WriteLine("worker_threads_busy: {0}", busyWorkerThreads);
+
+            Console.WriteLine("io_threads_min: {0}", minIoThreads);
+            Console.WriteLine("io_threads_max: {0}", maxIoThreads);
+
+            Console.WriteLine("io_threads_free: {0}", freeIoThreads);
+            Console.WriteLine("io_threads_busy: {0}", busyIoThreads);
+
             CreateHostBuilder(args).Build().Run();
         }
 
